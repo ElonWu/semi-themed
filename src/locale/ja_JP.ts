8d7be973d274fca1a6cc7f8a018024efbd78b4d2
吴渊 <wrzmonkey@gmail.com>
2021-12-03 14:54:36 +0800

    fix

--- src/locale/ja_JP.ts
+++ src/locale/ja_JP.ts
@@ -1,3 +1,15 @@
-const ja_JP = {};
+const ja_JP = {
+  table: {
+    pagination: ({
+      currentStart,
+      currentEnd,
+      total,
+    }: {
+      currentStart: number;
+      currentEnd: number;
+      total: number;
+    }) => `${currentStart} - ${currentEnd}项, 共${total}项`,
+  },
+};
 
 export default ja_JP;
